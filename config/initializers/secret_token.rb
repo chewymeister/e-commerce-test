7cd985e5e7f999c0e2989877f115f56f00240c9a
Jonathan Tsang <chewymeister88@googlemail.com>
2013-12-16 20:12:15 +0000

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EcommApp::Application.config.secret_key_base = '8a675137bd0e0ea19ec2e02947a9b7205f7c29720c155cd3bea86e0d469258bc2bf5e576a1db99129f447df2a99d415bb040573cf62b0a1e20be4dd4f530a37c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EcommApp::Application.config.session_store :cookie_store, key: '_ecomm_app_session'
