ebc2ee35188916f337115fbcfe6ca483d71d58fe
Jonathan Tsang <chewymeister88@googlemail.com>
2013-12-17 10:07:51 +0000

    GreenTest: link should redirect user to show page

--- spec/features/product/user_product_spec.rb
+++ spec/features/product/user_product_spec.rb
@@ -3,13 +3,23 @@ require 'spec_helper'
 describe "users product page" do
   let!(:product) { FactoryGirl.create(:product) }
 
-  context "User visits the home page" do
-    it "name should be visible to the user on the home page" do
+  context "When the User visits the home page" do
+    it "link to product should be visible to the user on the home page" do
       visit root_path
 
-      expect(page).to have_content "Product 1"
       expect(page).to have_link "Product 1"
     end
+
+    it "the link should redirect the user to the show page" do
+      visit root_path
+      click_link "Product 1"
+
+      expect(current_path).to eq product_path(product)
+    end
+  end
+
+  after do
+    FactoryGirl.reload
   end
 
 end
